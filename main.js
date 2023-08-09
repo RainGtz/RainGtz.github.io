c5a9ee7c068b9c5d9ada9866cab1c9ebdfcbffb9
Lluvia.Gutierrez <141576790+raingtz@users.noreply.github.com>
2023-08-09 17:13:09 -0600

    Primera Pagina de Internet
    Mi primera pagina de internet realizada en el programa Mujer Digital.

---
+++ main.js
@@ -0,0 +1,42 @@
+function muestra_oculta(id) {
+    let div = document.getElementById(id);
+    if (div.style.display == "none"){ 
+      div.style.display = "flex";
+    } 
+    else {
+      div.style.display = "none";
+    }
+}
+function muestra_oculta2(id) {
+    //declaro la funcion y paso por parametro el selector de referencia que quiero ocultar/mostrar
+    let div = document.getElementById(id);
+    //uso el condicional IF para saber si el DIV esta oculto (Display: none) o si esta visible 
+    if (div.style.display == "none"){ 
+      div.style.display = "flex";
+    } 
+    else {
+      div.style.display = "none";
+    }
+}
+function muestra_oculta3(id) {
+    //declaro la funcion y paso por parametro el selector de referencia que quiero ocultar/mostrar
+    let div = document.getElementById(id);
+    //uso el condicional IF para saber si el DIV esta oculto (Display: none) o si esta visible 
+    if (div.style.display == "none"){ 
+      div.style.display = "flex";
+    } 
+    else {
+      div.style.display = "none";
+    }
+}
+function muestra_oculta4(id) {
+    //declaro la funcion y paso por parametro el selector de referencia que quiero ocultar/mostrar
+    let div = document.getElementById(id);
+    //uso el condicional IF para saber si el DIV esta oculto (Display: none) o si esta visible 
+    if (div.style.display == "none"){ 
+      div.style.display = "flex";
+    } 
+    else {
+      div.style.display = "none";
+    }
+}
